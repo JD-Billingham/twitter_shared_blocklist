11be223fcb57ff31a31bad8a98070e691800ac77
ool0n <oolon@btinternet.com>
2013-06-11 14:47:58 +0100

    licence added

--- TBBV2/Web Root/show_blocks.php
+++ TBBV2/Web Root/show_blocks.php
@@ -4,6 +4,22 @@
  * User has successfully authenticated with Twitter. Access tokens saved to session and to filesystem as a user.
  * This shows the users blocks and the block list blocks -- what do they have already and what do they have that
  * the block list doesn't?
+ *     Licensed to the Apache Software Foundation (ASF) under one
+       or more contributor license agreements.  See the NOTICE file
+       distributed with this work for additional information
+       regarding copyright ownership.  The ASF licenses this file
+       to you under the Apache License, Version 2.0 (the
+       "License"); you may not use this file except in compliance
+       with the License.  You may obtain a copy of the License at
+
+         http://www.apache.org/licenses/LICENSE-2.0
+
+       Unless required by applicable law or agreed to in writing,
+       software distributed under the License is distributed on an
+       "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+       KIND, either express or implied.  See the License for the
+       specific language governing permissions and limitations
+       under the License.
  */
 /* Check domain is www.theblockbot.com -> if not forward to it */
 if ($_SERVER['HTTP_HOST']!="www.theblockbot.com") {
--- TBBV2/cleanup_blocklist.php
+++ TBBV2/cleanup_blocklist.php
@@ -4,7 +4,22 @@ require_once('/var/www/html/sign_up/config.php');
 require_once('/var/www/apbb/util.php');
 /**
  * Get blockees from /var/www/apbb/blocks/level_1/2/3
- * 
+ *     Licensed to the Apache Software Foundation (ASF) under one
+       or more contributor license agreements.  See the NOTICE file
+       distributed with this work for additional information
+       regarding copyright ownership.  The ASF licenses this file
+       to you under the Apache License, Version 2.0 (the
+       "License"); you may not use this file except in compliance
+       with the License.  You may obtain a copy of the License at
+
+         http://www.apache.org/licenses/LICENSE-2.0
+
+       Unless required by applicable law or agreed to in writing,
+       software distributed under the License is distributed on an
+       "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+       KIND, either express or implied.  See the License for the
+       specific language governing permissions and limitations
+       under the License.
  * Scan and if any names or ids change then reset the block list
  */
 #Start timer
