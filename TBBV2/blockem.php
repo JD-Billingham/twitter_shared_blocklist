9cc59b056e6bac3eb468df941fa8cd5c5c0e971d
ool0n <oolon@btinternet.com>
2013-09-30 23:47:36 +0100

    Update blockem.php

--- TBBV2/blockem.php
+++ TBBV2/blockem.php
@@ -1,11 +1,26 @@
 <?php
-#require_once('/var/www/html/sign_up/twitteroauth/twitteroauth.php');
-#require_once('/var/www/html/sign_up/config.php');
-#require_once('/var/www/apbb/util.php');
-# TEST REMOVE
-require_once('/var/www/html/the_block_bot/twitteroauth/twitteroauth.php');
-require_once('/var/www/html/the_block_bot/config.php');
-require_once('/var/www/the_block_bot/util.php');
+
+ /*     Licensed to the Apache Software Foundation (ASF) under one
+       or more contributor license agreements.  See the NOTICE file
+       distributed with this work for additional information
+       regarding copyright ownership.  The ASF licenses this file
+       to you under the Apache License, Version 2.0 (the
+       "License"); you may not use this file except in compliance
+       with the License.  You may obtain a copy of the License at
+
+         http://www.apache.org/licenses/LICENSE-2.0
+
+       Unless required by applicable law or agreed to in writing,
+       software distributed under the License is distributed on an
+       "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+       KIND, either express or implied.  See the License for the
+       specific language governing permissions and limitations
+       under the License.
+ */
+require_once('/var/www/html/sign_up/twitteroauth/twitteroauth.php');
+require_once('/var/www/html/sign_up/config.php');
+require_once('/var/www/apbb/util.php');
+
 /**
  * Get blockees from /var/www/the_block_bot/blocks/
  *  .... Block a maximum of x at a time and only those not blocked and not followed ....
