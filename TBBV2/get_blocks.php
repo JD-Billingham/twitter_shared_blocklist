a2393fe6adc65e29a77152085893d77b2476ccf5
ool0n <oolon@btinternet.com>
2013-09-30 23:47:51 +0100

    Update get_blocks.php

--- TBBV2/get_blocks.php
+++ TBBV2/get_blocks.php
@@ -2,6 +2,23 @@
 /**
 * @file
 * Reads the_block_bot mentions and any adds to block list or authorised users are processed
+* 
+ *     Licensed to the Apache Software Foundation (ASF) under one
+       or more contributor license agreements.  See the NOTICE file
+       distributed with this work for additional information
+       regarding copyright ownership.  The ASF licenses this file
+       to you under the Apache License, Version 2.0 (the
+       "License"); you may not use this file except in compliance
+       with the License.  You may obtain a copy of the License at
+
+         http://www.apache.org/licenses/LICENSE-2.0
+
+       Unless required by applicable law or agreed to in writing,
+       software distributed under the License is distributed on an
+       "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+       KIND, either express or implied.  See the License for the
+       specific language governing permissions and limitations
+       under the License.
 */
 require_once('/var/www/html/sign_up/twitteroauth/twitteroauth.php');
 require_once('/var/www/html/sign_up/config.php');
